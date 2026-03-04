cf9d1671bdf4f3556d0bb1b040d3f0c2fcc25785
KyleeM <124472184+Asya-789@users.noreply.github.com>
2026-03-04 12:16:57 -0600

    added flag and panning, almost done average

--- 10 Noise Terrain Gen/sketch.js
+++ 10 Noise Terrain Gen/sketch.js
@@ -10,48 +10,72 @@ let rectwidth = 10;
 let noiseY;
 let noiseTime = 1;
 let noiseSpeed = 0.01;
+let noisetSet = 1;
+let highestPointY = 0;
+let highestPointX = 0;
 
-let genheightArray = [];
+let noiseYArray = [];
+let noiseYAverage = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(5)
+  //frameRate(5)
   noStroke()
 }
 
 function draw() {
   background(0);
   genTerrain();
-  drawFlag()
 }
 
 function genTerrain() {
   //make a terrain that travels from side to side of screen and creates a panning like effect
-
+  highestPointY = windowHeight;
   //basic one 
   for (let i = 0; i <= windowWidth; i = i + rectwidth) {
     noiseY = map(noise(noiseTime), 0, 1, 0, windowHeight);
-    genheightArray.push(noiseY);
-    genheightArray.push(i);
-    noiseTime -= noiseSpeed;
+    noiseTime += noiseSpeed;
     rect(i, noiseY, rectwidth, windowHeight);
-  
-  }
 
+    if (noiseY < highestPointY) {
+      highestPointX = i;
+      highestPointY = noiseY;
+    }
+    noiseYArray.push(noiseY);
+  }
+  drawFlag();
+  average();
+  noiseYArray = [];
+  //for panning comment out to turn it off
+  noisetSet += 0.1;
+  noiseTime = noisetSet;
 }
 
 function drawFlag() {
   //draw flag at highest peak
-  fill(50);
-  rect(genheightArray[highest + 1], genheightArray[highest]+ 10, 3, 10)
-  fill(0, 255, 0)
+  fill(150);
+  rect(highestPointX, highestPointY, 3, -50);
+  fill(0, 255, 0);
+  rect(highestPointX + 3, highestPointY - 50, 30, 20);
+  fill(255);
 }
 
 function keyPressed() {
   if (keyCode == 38) {
-    rectwidth++
+    rectwidth++;
   }
   else if (keyCode == 40 && rectwidth != 1) {
-    rectwidth--
+    rectwidth--;
   }
+}
+
+function average() {
+  for(let i = 0; i <= noiseYArray.length; i++){
+    noiseYAverage = noiseYAverage + noiseYArray[i];
+  }
+  noiseYAverage = noiseYAverage / noiseYArray.length
+  text(noiseYArray.length, 20, 20)
+  fill(255, 0, 0);
+  rect(0, noiseYAverage, windowWidth, 5)
+  fill(255)
 }
