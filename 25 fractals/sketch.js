45c80fd746b8238fa9d12934aeeec20d0fd3b70b
KyleeM <124472184+Asya-789@users.noreply.github.com>
2026-05-04 12:05:49 -0600

    Update sketch.js

--- 25 fractals/sketch.js
+++ 25 fractals/sketch.js
@@ -8,8 +8,38 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  noFill();
 }
 
 function draw() {
   background(220);
+  //centerCircle(width/2, height/2, 100);
+  //fractalCircle(width/2, height/2, 500);
+  luckySquare(width/2, height/2, width/2)
+}
+
+function centerCircle(x,y,d){
+  if(d > 5){
+    circle(x,y,d)
+    centerCircle(x,y,d* 0.9)
+  }
+}
+function fractalCircle(x,y,d){
+  if(d > 1){
+    circle(x,y,d)
+    fractalCircle(x - d/2, y, d* 0.5)
+    fractalCircle(x + d/2, y, d* 0.5)
+    //fractalCircle(x, y - d/2, d* 0.5)
+    fractalCircle(x, y + d/2,  d* 0.5)
+  }
+}
+function luckySquare(x,y,s){
+  if(s > 5){
+    square(x,y,s)
+    luckySquare(x-s/2, y-s/2, s*0.45);
+    luckySquare(x+s/2, y-s/2, s*0.45);
+    luckySquare(x-s/2, y+s/2, s*0.45);
+    luckySquare(x+s/2, y+s/2, s*0.45);
+  }
 }
